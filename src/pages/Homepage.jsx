de5ea10a99ea170d186ee98f0c86ee746982cc22
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-09-22 16:53:52 +0200

    feat: milestone-3

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,7 +9,7 @@ const Homepage = () => {
 			.get("http://localhost:3000/api/movies/")
 			.then((resp) => {
 				setMovies(resp.data);
-				console.log(resp);
+				console.log(resp.data);
 			})
 			.catch((err) => console.log(err));
 	};
@@ -25,19 +25,22 @@ const Homepage = () => {
 					<div className="col-12">
 						<h1>Film disponibili</h1>
 					</div>
-					<div className="col-12">
-						{}
-						<div className="card">
-							<img src="..." className="card-img-top" alt="..." />
-							<div className="card-body">
-								<h5 className="card-title">Card title</h5>
-								<p className="card-text">Some quick example text.</p>
-								<a href="#" className="btn btn-primary">
-									Go somewhere
-								</a>
+					{movies.map((movie) => {
+						return (
+							<div className="col-12 col-md-6 col-lg-4" key={movie.id}>
+								<div className="card">
+									<img src="..." className="card-img-top" alt={movie.title} />
+									<div className="card-body">
+										<h5 className="card-title">{movie.title}</h5>
+										<p className="card-text">{movie.director}</p>
+										<p className="card-text">{movie.genre}</p>
+										<p className="card-text">{movie.reales_year}</p>
+									</div>
+								</div>
+								;
 							</div>
-						</div>
-					</div>
+						);
+					})}
 				</div>
 			</div>
 		</div>
