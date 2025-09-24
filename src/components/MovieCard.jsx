fa11e0b0f19279566d7a00b349901d9bc32985d5
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-09-24 23:54:57 +0200

    fix: card-img

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,13 +6,7 @@ const MovieCard = ({ movie }) => {
 		<div className="col-12 col-md-6 col-lg-4" key={movie.id}>
 			<Link to={`/movies/${movie.id}`}>
 				<div className="card my-3">
-					<img src={movie.image} className="card-img-top" alt={movie.title} />
-					<div className="card-body">
-						<h5 className="card-title">{movie.title}</h5>
-						<p className="card-text">{movie.director}</p>
-						<p className="card-text">{movie.genre}</p>
-						<p className="card-text">{movie.release_year}</p>
-					</div>
+					<img src={movie.image} className="card-img" alt={movie.title} />
 				</div>
 			</Link>
 		</div>
